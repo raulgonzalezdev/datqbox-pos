759af1ef0673c65860f441c9c481d2383cb03346
Raul Gonzalez <gq.raul@gmail.com>
2023-05-04 23:01:00 -0400

    arccordion en sidebar

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,3 @@
-
 import Dashboard from "views/Dashboard/Dashboard.js";
 import Tables from "views/Dashboard/Tables.js";
 import Billing from "views/Dashboard/Billing.js";
@@ -6,10 +5,10 @@ import RTLPage from "views/RTL/RTLPage.js";
 import Profile from "views/Dashboard/Profile.js";
 import SignIn from "views/Pages/SignIn.js";
 import SignUp from "views/Pages/SignUp.js";
-import StepperForm from "views/Pages/StepperForm/StepperForm.js"
-import UsersList from "views/Pages/StepperForm/UsersList"
-import PeriodsAccounting from "views/Pages/PeriodsAccounting/PeriodsAccounting.js"
-import POSApp from "views/Pages/POSApp/POSApp.js"
+import StepperForm from "views/Pages/StepperForm/StepperForm.js";
+import UsersList from "views/Pages/StepperForm/UsersList";
+import PeriodsAccounting from "views/Pages/PeriodsAccounting/PeriodsAccounting.js";
+import POSApp from "views/Pages/POSApp/POSApp.js";
 
 import {
   HomeIcon,
@@ -19,97 +18,140 @@ import {
   DocumentIcon,
   RocketIcon,
   SupportIcon,
-  
-  
 } from "components/Icons/Icons";
+
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+} from "@chakra-ui/react";
 
 var dashRoutes = [
   {
     path: "/dashboard",
     name: "Dashboard",
     rtlName: "لوحة القيادة",
-    icon: <HomeIcon color='inherit' />,
+    icon: <HomeIcon color="inherit" />,
     component: Dashboard,
-    layout: "/admin",
-  },
-  {
-    path: "/Userslist",
-    name: "User List",
-    rtlName: "لوحة القيادة",
-    icon: <StatsIcon color='inherit' />,
-    component: UsersList,
-    layout: "/admin",
-  },
-  {
-    path: "/tables",
-    name: "Tables",
-    rtlName: "لوحة القيادة",
-    icon: <StatsIcon color='inherit' />,
-    component:Tables,
     layout: "/admin",
   },
   {
     path: "/pos",
     name: "POS",
     rtlName: "لوحة القيادة",
-    icon: <CreditIcon color='inherit' />,
+    icon: <CreditIcon color="inherit" />,
     component: POSApp,
     layout: "/pos",
   },
-  
+
+  // {
+  //   name: "Modulos",
+  //   category: "account",
+  //   rtlName: "صفحات",
+  //   state: "pageCollapse",
+  //   views: [],
+  //   // views: [
+  //   //   {
+  //   //     path: "/Userslist",
+  //   //     name: "User List",
+  //   //     rtlName: "لوحة القيادة",
+  //   //     icon: <StatsIcon color="inherit" />,
+  //   //     component: UsersList,
+  //   //     layout: "/admin",
+  //   //   },
+  //   //   {
+  //   //     path: "/tables",
+  //   //     name: "Tables",
+  //   //     rtlName: "لوحة القيادة",
+  //   //     icon: <StatsIcon color="inherit" />,
+  //   //     component: Tables,
+  //   //     layout: "/admin",
+  //   //   },
+  //   // ],
+  // },
   {
-    path: "/periodsaccounting",
-    name: "Periods Accounting",
-    rtlName: "آرتيإل",
-    icon: <SupportIcon color='inherit' />,
-    component: PeriodsAccounting,
-    layout: "/admin",
-  },
-  {
-    path: "/rtl-support-page",
-    name: "RTL",
-    rtlName: "آرتيإل",
-    icon: <SupportIcon color='inherit' />,
-    component: RTLPage,
-    layout: "/rtl",
-  },
-  {
-    name: "ACCOUNT PAGES",
+    name: "Pedidos",
     category: "account",
     rtlName: "صفحات",
-    state: "pageCollapse",
+    state: "pageArcordion",
+    icon: <CreditIcon color="inherit" />,
+    views: [
+      {
+        path: "/periodsaccounting",
+        name: "Clientes",
+        rtlName: "آرتيإل",
+        icon: <SupportIcon color="inherit" />,
+        secondaryNavbar: true,
+        component: PeriodsAccounting,
+        layout: "/admin",
+      },
+      {
+        path: "/rtl-support-page",
+        name: "Pedidos",
+        rtlName: "آرتيإل",
+        icon: <SupportIcon color="inherit" />,
+        component: RTLPage,
+        layout: "/admin",
+      },
+      {
+        path: "/rtl-support",
+        name: "Pagos",
+        rtlName: "آرتيإل",
+        icon: <SupportIcon color="inherit" />,
+        component: Tables,
+        layout: "/admin",
+      },
+    ],
+  },
+  {
+    name: "Productos",
+    category: "account",
+    rtlName: "صفحات",
+    icon: <CreditIcon color="inherit" />,
+    state: "pageArcordion",
     views: [
       {
         path: "/profile",
-        name: "Profile",
+        name: "Articulos",
         rtlName: "لوحة القيادة",
-        icon: <PersonIcon color='inherit' />,
+        icon: <PersonIcon color="inherit" />,
         secondaryNavbar: true,
         component: Profile,
         layout: "/admin",
       },
+    ],
+  },
+  {
+    name: "Reportes",
+    category: "account",
+    rtlName: "صفحات",
+    icon: <CreditIcon color="inherit" />,
+    state: "pageArcordion",
+    views: [
       {
         path: "/stepperform",
-        name: "StepperForm",
+        name: "Detalle de Ventas",
         rtlName: "لوحة القيادة",
-        icon: <PersonIcon color='inherit' />,
+        icon: <PersonIcon color="inherit" />,
         secondaryNavbar: true,
         component: StepperForm,
         layout: "/admin",
       },
       {
         path: "/signin",
-        name: "Sign In",
+        name: "Pedidos",
         rtlName: "لوحة القيادة",
-        icon: <DocumentIcon color='inherit' />,
+        icon: <DocumentIcon color="inherit" />,
         component: SignIn,
         layout: "/auth",
       },
       {
         path: "/signup",
-        name: "Sign Up",
+        name: "Formas de Pago",
         rtlName: "لوحة القيادة",
-        icon: <RocketIcon color='inherit' />,
+        icon: <RocketIcon color="inherit" />,
         secondaryNavbar: true,
         component: SignUp,
         layout: "/auth",
